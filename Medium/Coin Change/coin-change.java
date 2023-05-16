cd9ef0f34e11379825b908f2b6df3185623dbdec
KALVA AKHIL REDDY <101055464+kalvaakhil@users.noreply.github.com>
2023-05-16 10:04:41 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Coin Change/coin-change.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int sum = sc.nextInt();
+            int N = sc.nextInt();
+            int coins[] = new int[N];
+            for (int i = 0; i < N; i++) coins[i] = sc.nextInt();
+            Solution ob = new Solution();
+            System.out.println(ob.count(coins, N, sum));
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+
+
+// User function Template for Java
+
+class Solution {
+    public long count(int coins[], int N, int sum) {
+        long[] dp=new long[sum+1];
+        dp[0]=1;
+        for(int i=0;i<coins.length;i++){
+            int val=coins[i];
+            for(int j=1;j<=sum;j++){
+                if(val<=j){
+                    dp[j]+=dp[j-val];
+                }
+            }
+        }
+        return dp[sum];
+    }
+}
