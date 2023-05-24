ea9d487d271b23bbb9dd8022ada6a7c4690b3986
KALVA AKHIL REDDY <101055464+kalvaakhil@users.noreply.github.com>
2023-05-24 09:52:48 +0530

    Added solution - LeetHub

--- BFS of graph - GFG/bfs-of-graph.java
+++ BFS of graph - GFG/bfs-of-graph.java
@@ -33,33 +33,28 @@ class GFG {
 // } Driver Code Ends
 
 
+
+
 class Solution {
     // Function to return Breadth First Traversal of given graph.
     public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
         // Code here
-        ArrayList < Integer > bfs = new ArrayList < > ();
-        boolean vis[] = new boolean[V];
-        Queue < Integer > q = new LinkedList < > ();
-
+        ArrayList<Integer> ans=new ArrayList<Integer>();
+        Queue<Integer> q=new LinkedList<>();
+        boolean visited[]=new boolean[V];
+        visited[0]=true;
         q.add(0);
-        vis[0] = true;
-
-        while (!q.isEmpty()) {
-            Integer node = q.poll();
-            bfs.add(node);
-
-            // Get all adjacent vertices of the dequeued vertex s
-            // If a adjacent has not been visited, then mark it
-            // visited and enqueue it
-            for (Integer it: adj.get(node)) {
-                if (vis[it] == false) {
-                    vis[it] = true;
+        while(!q.isEmpty()){
+            Integer node=q.poll();
+            ans.add(node);
+            for(Integer it:adj.get(node)){
+                if(visited[it]==false){
+                    visited[it]=true;
                     q.add(it);
                 }
             }
-        }
-
-        return bfs;
+            }
+            return ans;
         
     }
 }
