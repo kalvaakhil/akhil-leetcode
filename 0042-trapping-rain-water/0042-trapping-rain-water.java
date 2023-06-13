9ffb4af7601fffc221e88647722bafe5a343a4c8
KALVA AKHIL REDDY <101055464+kalvaakhil@users.noreply.github.com>
2023-06-13 15:36:57 +0530

    Time: 1 ms (97.27%), Space: 45 MB (5.91%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int trap(int[] h) {
+        int l=0,r=h.length-1,lmax=0,rmax=0,water=0;
+        while(l<=r){
+            lmax=Math.max(lmax,h[l]);
+            rmax=Math.max(rmax,h[r]);
+            water+=(lmax<rmax)? lmax-h[l++]:rmax-h[r--];
+        }
+        return water;
+    }
+}
