234111482b3c580334263bd5b815b235ebf4f95c
KALVA AKHIL REDDY <101055464+kalvaakhil@users.noreply.github.com>
2023-06-25 08:50:20 +0530

    Added solution - LeetHub

---
+++ Unique rows in boolean matrix - GFG/unique-rows-in-boolean-matrix.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+import java.lang.Math;
+import java.io.*;
+import java.util.*;
+class UniqueRows{
+public static void main(String[] args)throws IOException
+ {  
+     BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+     PrintWriter out=new PrintWriter(System.out);
+		int t=Integer.parseInt(in.readLine().trim());
+		while(t-->0){
+		    String s[]=in.readLine().trim().split(" ");
+			int r=Integer.parseInt(s[0]);
+			int c=Integer.parseInt(s[1]);
+			s=in.readLine().trim().split(" ");
+			int ind=0;
+			int[][] a=new int [r][c];
+			for(int i=0;i<r;i++)
+				for(int j=0;j<c;j++)
+					a[i][j]=Integer.parseInt(s[ind++]);
+			GfG g=new GfG();
+			ArrayList<ArrayList<Integer>> arr = g.uniqueRow(a,r,c);
+			for(int i = 0;i<arr.size();i++){
+			    ArrayList<Integer> demo = arr.get(i);
+			    for(int j = 0;j<demo.size();j++){
+			        out.print(demo.get(j)+" ");
+			    }
+			    out.print("$");
+			}
+			out.println();
+		}
+		out.close();
+ }
+}
+// } Driver Code Ends
+
+
+/*Complete the given function*/
+class GfG
+{
+    public static ArrayList<ArrayList<Integer>> uniqueRow(int a[][],int r, int c)
+    {
+        //add code here.
+                ArrayList<Integer> list=new ArrayList<>();
+        HashSet<Integer> set=new HashSet<>();
+        ArrayList<ArrayList<Integer>>list1=new ArrayList<>();
+        int dec=0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                dec=((2 * dec)+a[i][j]);
+                list.add(a[i][j]);
+            }
+            if (!(set.contains(dec)))
+            {
+                list1.add(new ArrayList<>(list));
+                set.add(dec);
+            }
+            list.clear();
+            dec=0;
+        }
+        return list1;
+    }
+}
