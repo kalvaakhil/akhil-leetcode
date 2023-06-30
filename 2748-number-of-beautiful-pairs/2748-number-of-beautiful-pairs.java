79d3c64f71385e38b210acbc2232f5ea5a8407de
KALVA AKHIL REDDY <101055464+kalvaakhil@users.noreply.github.com>
2023-06-30 09:52:30 +0530

    Time: 27 ms (82.76%), Space: 43.5 MB (60.68%) - LeetHub

--- 2748-number-of-beautiful-pairs/2748-number-of-beautiful-pairs.java
+++ 2748-number-of-beautiful-pairs/2748-number-of-beautiful-pairs.java
@@ -1,38 +1,20 @@
 class Solution {
-    private int gcd(int a,int b){
-        while(a>0 && b>0){
-            if(a>b){
-                a=a%b;
-            }
-            else{
-                b=b%a;
-            }
-        }
-        if(a>0){
-            return a;
-        }
-        return b;
-    }
-    private int getfirst(int n){
-        int ls=0;
-        while(n>0){
-            ls=n%10;
-            n/=10;
-        }
-        return ls;
-    }
     public int countBeautifulPairs(int[] nums) {
-        int n=nums.length;
-        int c=0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int ld=nums[j]%10;
-                int fd=getfirst(nums[i]);
-                if(gcd(ld,fd)==1){
-                    c++;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int firstDigit = nums[i];
+            while (firstDigit > 9) firstDigit /= 10;
+            for (int j = i + 1; j < nums.length; j++) {
+                int lastDigit = nums[j] % 10;
+                if (gcd(firstDigit, lastDigit) == 1) {
+                    count++;
                 }
             }
         }
-        return c;
+        return count;
+    }
+    public int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
     }
 }
