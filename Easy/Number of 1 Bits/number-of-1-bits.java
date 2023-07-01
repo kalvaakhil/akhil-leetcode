31f76960c51c14f6cfcf291f2c8855970d770629
KALVA AKHIL REDDY <101055464+kalvaakhil@users.noreply.github.com>
2023-07-01 16:02:50 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Number of 1 Bits/number-of-1-bits.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int N = sc.nextInt();
+
+            Solution ob = new Solution();
+            int cnt = ob.setBits(N);
+            System.out.println(cnt);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    static int setBits(int N) {
+        // code here
+        String x=Integer.toBinaryString(N);
+        int j,c=0;
+        for(j=0;j<x.length();j++){
+            char i=x.charAt(j);
+            if(i=='1'){
+                c++;
+            }
+        }
+        return c;
+    }
+}
