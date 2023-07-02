00f129e57e432b39162274d55f73a211347fc723
KALVA AKHIL REDDY <101055464+kalvaakhil@users.noreply.github.com>
2023-07-02 12:16:10 +0530

    Added solution - LeetHub

---
+++ Set all the bits in given range of a number - GFG/set-all-the-bits-in-given-range-of-a-number.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            
+            int N = Integer.parseInt(S[0]);
+            int L = Integer.parseInt(S[1]);
+            int R = Integer.parseInt(S[2]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.setAllRangeBits(N,L,R));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int setAllRangeBits(int N , int L , int R) {
+        // code here
+        for(int i=L-1;i<=R-1;i++){
+            N|=(1<<i);
+        }
+        return N;
+    }
+};
