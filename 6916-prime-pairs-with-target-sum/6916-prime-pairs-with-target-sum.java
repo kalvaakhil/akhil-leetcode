352f73f0145f52cd8fac574b77f2a5b94400cc46
KALVA AKHIL REDDY <101055464+kalvaakhil@users.noreply.github.com>
2023-07-02 13:20:26 +0530

    Time: 242 ms (100.00%), Space: 54.7 MB (25.00%) - LeetHub

--- 6916-prime-pairs-with-target-sum/6916-prime-pairs-with-target-sum.java
+++ 6916-prime-pairs-with-target-sum/6916-prime-pairs-with-target-sum.java
@@ -1,37 +1,36 @@
 class Solution {
-    boolean a[]=new boolean[1000001];
-    public boolean isprime(int n){
-        Arrays.fill(a,true);
-        a[0]=false;
-        a[1]=false;
-        for(int i=2;i*i<=n;i++){
-            if(a[i]==true){
-                for(int j=i*i;j<=n;j+=i)
-                {
-                    a[j]=false;
+    public List<List<Integer>> findPrimePairs(int n) {
+        boolean[] prime = new boolean[n + 1]; // Array to store whether a number is prime or not
+        List<List<Integer>> ans = new ArrayList<>(); // List to store the pairs of prime numbers
+        
+        // Initializing all numbers as prime
+        for (int i = 2; i <= n; i++) {
+            prime[i] = true;
+        }
+        
+        prime[1] = false; // 1 is not a prime number
+        prime[0] = false; // 0 is not a prime number
+        
+        // Finding prime numbers using the Sieve of Eratosthenes algorithm
+        for (int p = 2; p * p <= n; p++) {
+            if (prime[p]) {
+                // Marking multiples of p as non-prime
+                for (int i = p * p; i <= n; i += p) {
+                    prime[i] = false;
                 }
             }
         }
-        return a[n];
-    }
-    public List<List<Integer>> findPrimePairs(int n) {
-        List<List<Integer>> ans=new ArrayList<>();
-        int i;
-        int x=(int)Math.pow(10,6);
-        isprime(x);
-        for(i=2;i<=n/2;i++){
-            if((n-i)<0)
-            {
-                continue;
-            } 
-            if(a[i]==true && a[n-i]==true){
-                List<Integer> t=new ArrayList<>();
-                t.add(i);
-                t.add(n-i);
-                Collections.sort(t);
-                ans.add(t);
+        
+        for (int i = 2; i <= n - 1; i++) {
+            int j = n - i; // Finding the complement of i to make the sum n
+            if (prime[i] && prime[j] && i <= j) {
+                List<Integer> temp = new ArrayList<>(); // Creating a pair of prime numbers
+                temp.add(i);
+                temp.add(j);
+                ans.add(temp); // Adding the pair to the result list
             }
         }
-        return ans;
+        
+        return ans; // Returning the list of prime pairs
     }
 }
