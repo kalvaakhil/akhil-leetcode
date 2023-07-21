0e6305bcd522267dc61af582315250772b10de5c
KALVA AKHIL REDDY <101055464+kalvaakhil@users.noreply.github.com>
2023-07-21 09:04:18 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Remove all occurences of duplicates in a linked list/remove-all-occurences-of-duplicates-in-a-linked-list.java
@@ -0,0 +1,111 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+class duplicates
+{
+    Node head;  
+    Node tail;
+	public void addToTheLast(Node node) 
+	{
+	  if (head == null) 
+	  {
+	   head = node;
+	   tail = node;
+	  } 
+	  else 
+	  {
+	   tail.next = node;
+	   tail = node;
+	  }
+	}
+      void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	/* Drier program to test above functions */
+	public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+         {
+			int N = sc.nextInt();
+			duplicates llist = new duplicates(); 
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < N; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+			
+        Solution ob = new Solution();
+		head = ob.removeAllDuplicates(llist.head);
+		llist.printList(head);
+		
+        t--;		
+        }
+    }
+    
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* Linked List Node class
+
+class Node
+{
+    int data;
+    Node next;
+}
+    
+*/
+
+class Solution
+{
+    public static Node removeAllDuplicates(Node head)
+    {
+        //code here
+        LinkedHashMap<Integer,Integer> map = new LinkedHashMap<>();
+        
+        Node tmp = head;
+        while(tmp != null){
+            map.put(tmp.data,map.getOrDefault(tmp.data,0) + 1);
+            tmp = tmp. next;
+        }
+        
+        Node head2 = null;
+        Node tail = null;
+        
+        for(Integer key : map.keySet()){
+            if(map.get(key) <= 1){
+                Node newNode = new Node(key);
+                if(head2 == null){
+                    head2 = tail = newNode;
+                }
+                else{
+                    tail.next = newNode;
+                    tail = newNode;
+                }
+            }
+        }
+        
+        return head2;
+    }
+}
