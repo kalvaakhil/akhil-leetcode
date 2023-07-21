d35728c30f5d522025f7aa6518db128d462a8f73
KALVA AKHIL REDDY <101055464+kalvaakhil@users.noreply.github.com>
2023-07-21 15:35:31 +0530

    Time: 12 ms (98.88%), Space: 59.4 MB (81.81%) - LeetHub

--- 2750-ways-to-split-array-into-good-subarrays/2750-ways-to-split-array-into-good-subarrays.java
+++ 2750-ways-to-split-array-into-good-subarrays/2750-ways-to-split-array-into-good-subarrays.java
@@ -1,25 +1,34 @@
 class Solution {
-    public int numberOfGoodSubarraySplits(int[] nums) {
-        int count=0;
-        long ans=1;
-        int check_1=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                if(check_1==1)
-                    count++;
-            }
-            else{
-                if(check_1==0){
-                   check_1=1;
-                    count++;
-                }
-                else{
-                    ans=(ans*count)%1000000007;
-                    count=1;
-                }
-            }
-        }
-        if(count==0) return 0;
-        return (int)ans;
+  private static final int modulo = (int) 1e9 + 7;
+
+  public int numberOfGoodSubarraySplits(int[] nums) {
+    final int n = nums.length;
+    long count = 0;
+
+    // find the first one
+    int i = 0;
+    while (i < n && nums[i] != 1) {
+      i++;
     }
+
+    if (i < n) {
+      count++;
+    }
+
+    for (int j = i + 1; j < n; j++) {
+      while (j < n && nums[j] == 0) {
+        j++;
+      }
+
+      if (j < n && nums[j] == 1) {
+        // we have j - i cuts with zero between two one(i, j)
+        // 10...01
+        // i ... j
+        count = count * (j - i) % modulo;
+        i = j;
+      }
+    }
+
+    return (int) count;
+  }
 }
