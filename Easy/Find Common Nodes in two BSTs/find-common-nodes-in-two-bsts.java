8a6b7b8639b305bb0115a5056f2812b20d42bf51
KALVA AKHIL REDDY <101055464+kalvaakhil@users.noreply.github.com>
2023-07-28 09:03:15 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find Common Nodes in two BSTs/find-common-nodes-in-two-bsts.java
@@ -0,0 +1,144 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class Node{
+    int data;
+    Node left, right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+class Solution
+{
+    //Function to find the nodes that are common in both BST.
+    public static void inOrder2(Node root, ArrayList<Integer> ar){
+        if(root != null){
+             inOrder2(root.left, ar);
+             ar.add(root.data);
+             inOrder2(root.right, ar);
+        }
+    }
+    public static ArrayList<Integer> findCommon(Node root1,Node root2)
+    {
+        //code here
+        ArrayList<Integer> ar = new ArrayList<>();
+        ArrayList<Integer> ar1 = new ArrayList<>();
+        ArrayList<Integer> ar2 = new ArrayList<>();
+        
+        inOrder2(root1, ar1);
+        inOrder2(root2, ar2);
+        if(ar1.size() < ar1.size()){
+            for(int i = 0; i<ar1.size();i++){
+                if((ar2.contains(ar1.get(i))))
+                    ar.add(ar1.get(i));
+            }
+            return ar;
+        }
+        else{
+            for(int i = 0; i<ar2.size();i++){
+                if((ar1.contains(ar2.get(i))))
+                    ar.add(ar2.get(i));
+            }
+            return ar;
+        }
+    }
+    
+}
+
+
+//{ Driver Code Starts.
+class GFG
+{
+    static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length() == 0 || str.equals('N'))
+            return null;
+        String[] s = str.split(" ");
+        
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue <Node> q = new LinkedList<Node>();
+        q.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(!q.isEmpty() && i < s.length)
+        {
+              // Get and remove the front of the queue
+              Node currNode = q.remove();
+        
+              // Get the current node's value from the string
+              String currVal = s[i];
+        
+              // If the left child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the left child for the current node
+                  currNode.left = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.left);
+              }
+        
+              // For the right child
+              i++;
+              if(i >= s.length)
+                  break;
+              currVal = s[i];
+        
+              // If the right child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the right child for the current node
+                  currNode.right = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.right);
+              }
+              
+              i++;
+        }
+    
+        return root;
+    }
+    
+    public static void main(String args[]) throws IOException {
+    
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t>0)
+        {
+            String s = br.readLine();
+            Node root1 = buildTree(s);
+            
+            s = br.readLine();
+            Node root2 = buildTree(s);
+            
+            Solution g = new Solution();
+            ArrayList<Integer> res = new ArrayList<Integer>();
+            res = g.findCommon(root1,root2);
+            for(int i = 0;i<res.size();i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println();
+            
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
