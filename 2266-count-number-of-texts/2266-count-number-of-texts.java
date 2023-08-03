e4989b39a2879d6300324e668fb732e0921d5bd0
KALVA AKHIL REDDY <101055464+kalvaakhil@users.noreply.github.com>
2023-08-03 09:01:22 +0530

    Time: 21 ms (91.67%), Space: 44.7 MB (47.92%) - LeetHub

---
+++ 2266-count-number-of-texts/2266-count-number-of-texts.java
@@ -0,0 +1,22 @@
+class Solution {
+    int mod = (1000000007);
+
+    public int countTexts(String pressedKeys) {
+        int[] key = new int[] { 0, 0, 3, 3, 3, 3, 3, 4, 3, 4 };
+        int n = pressedKeys.length();
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            int count = 0;
+            int num = pressedKeys.charAt(ind) - '0';
+            int rep = key[num];
+            for (int i = 0; i < rep && ind + i < pressedKeys.length() && pressedKeys.charAt(ind) == pressedKeys.charAt(ind + i); i++) {
+                count += dp[ind+i+1];
+                count %= mod;
+            }
+             dp[ind] = count;
+        }
+        return dp[0];
+    }
+}
