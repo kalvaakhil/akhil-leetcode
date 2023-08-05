6d942abf3a4c2d2957ba03b9b9bd6cd504cd118d
KALVA AKHIL REDDY <101055464+kalvaakhil@users.noreply.github.com>
2023-08-05 11:17:12 +0530

    Added solution - LeetHub

---
+++ Shop in Candy Store - GFG/shop-in-candy-store.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(input_line[0]);
+            int K = Integer.parseInt(input_line[1]);
+            input_line = read.readLine().trim().split("\\s+");
+            int candies[]= new int[N];
+            for(int i = 0; i < N; i++)
+                candies[i] = Integer.parseInt(input_line[i]);
+
+            Solution ob = new Solution();
+            ArrayList<Integer> cost = ob.candyStore(candies,N,K);
+            System.out.println(cost.get(0)+" "+cost.get(1));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution{
+    static ArrayList<Integer> candyStore(int candies[],int N,int K){
+        // code here
+        Arrays.sort(candies);
+        ArrayList list=new ArrayList<>();
+        
+        int count=N,i=0,j=N-1;
+        int cost1=0,cost2=0;
+        while(count>0){
+            cost1+=candies[i++];
+            cost2+=candies[j--];
+            count-=(K+1);
+        }
+        list.add(cost1);
+        list.add(cost2);
+        return list;
+    }
+}
