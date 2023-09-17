684723b4340b7127679fdb5d5740fb72efc4d7c3
KALVA AKHIL REDDY <101055464+kalvaakhil@users.noreply.github.com>
2023-09-17 17:19:49 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Divide the number/divide-the-number.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.countWays(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static long countWays(int n){
+        // code here
+        long cnt=0;
+        for(int i=1; i<=n; i++){
+            for(int j=i; j<=n; j++){
+                for(int k=j; k<=n; k++){
+                    if((n-(i+j+k))>=k){
+                        cnt++;
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+}
