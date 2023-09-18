6c03a56320ca297c661b62720129da762c2b3c8c
KALVA AKHIL REDDY <101055464+kalvaakhil@users.noreply.github.com>
2023-09-18 16:17:35 +0530

    Added solution - LeetHub

---
+++ 1s Complement - GFG/1s-complement.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String S = read.readLine();
+          
+
+            Solution ob = new Solution();
+            System.out.println(ob.onesComplement(S,N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static String onesComplement(String S,int N){
+        //code here
+        String str ="";
+       for(int i=0;i< S.length();i++){
+           if( S.charAt(i)== 49){
+               str += "0";
+           }
+           else{
+               str+= "1";
+           }
+       }
+       return str;
+    }
+}
+
