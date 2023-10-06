5641e0ed8935bab04e3167e3022c8b0df0c4ca64
KALVA AKHIL REDDY <101055464+kalvaakhil@users.noreply.github.com>
2023-10-06 10:41:43 +0530

    Time: 1590 ms (84.69%), Space: 0B (100.00%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -2,7 +2,7 @@ Select u.name,IFNULL(sum(r.distance),0) as travelled_distance
 from Users u
 LEFT JOIN Rides r
 ON u.id=r.user_id 
-GROUP BY r.user_id
+GROUP BY u.id
 order by travelled_distance desc,
 name asc;
 # SELECT Users.name, IFNULL(SUM(Rides.distance), 0) AS travelled_distance
