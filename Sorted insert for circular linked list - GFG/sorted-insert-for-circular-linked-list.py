1e479ae709505c0a093de650d99eed323aebeaad
KALVA AKHIL REDDY <101055464+kalvaakhil@users.noreply.github.com>
2023-10-08 10:44:24 +0530

    Added solution - LeetHub

---
+++ Sorted insert for circular linked list - GFG/sorted-insert-for-circular-linked-list.py
@@ -0,0 +1,104 @@
+#User function Template for python3
+
+'''
+class Node: 
+    # Constructor to initialize the node object 
+    def __init__(self, data): 
+        self.data = data 
+        self.next = None
+  '''
+class Solution:
+    def sortedInsert(self, head, data):
+        new_node = Node(data)
+        #if list is empty
+        if not head:
+            new_node.next = new_node
+            head = new_node
+            return head
+            
+        #insert at the begining    
+        if data < head.data:
+            new_node.next = head
+            temp = head
+            while temp.next != head:
+                temp = temp.next
+            temp.next = new_node
+            head = new_node
+            return head
+        
+        #insert in between
+        temp = head
+        while temp:
+            if temp.data <= data and temp.next.data>data:
+                new_node.next = temp.next
+                temp.next = new_node
+                return head
+            temp =temp.next
+        
+        #insert at the last
+        new_node.next = head
+        temp.next = new_node
+        return head
+
+
+#{ 
+ # Driver Code Starts
+#contributed by RavinderSinghPB
+class Node: 
+    # Constructor to initialize the node object 
+    def __init__(self, data): 
+        self.data = data 
+        self.next = None
+  
+class LinkedList: 
+  
+    # Function to initialize head 
+    def __init__(self): 
+        self.head = None
+        self.last=None
+  
+    # Function to insert a new node  
+    def push(self, data):
+        if not self.head:
+            nn=Node(data)
+            self.head=nn
+            self.last=nn
+            nn.next=nn
+            return
+        nn=Node(data)
+        nn.next=self.head
+        self.last.next=nn
+        self.last=nn 
+  
+
+# Utility function to print the linked LinkedList
+
+def printList(head):
+    if not head:
+        return
+    temp = head 
+    print (temp.data,end=' ') 
+    temp = temp.next
+    while(temp != head): 
+        print (temp.data,end=' ') 
+        temp = temp.next
+  
+    
+if __name__ =='__main__':
+    t=int(input())
+    for tcs in range(t):
+        n=int(input())
+        arr=[int(x) for x in input().split()]
+        data=int(input())
+
+        cll=LinkedList()
+        for e in arr:
+            cll.push(e)
+            
+        reshead=Solution().sortedInsert(cll.head,data)
+        printList(reshead)
+        print()
+        
+
+
+# } Driver Code Ends
